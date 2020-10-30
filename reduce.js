b12ea2c9ed10c342ff53dc4010e1bc40323bbf48
CodeLeviosa <emmanuelfabiani23@gmail.com>
2020-10-30 09:06:35 +0000

    added for of

--- reduce.js
+++ reduce.js
@@ -5,43 +5,27 @@ const items = [{
   {
     name: "TV",
     price: 200
-  },
-  {
-    name: "Album",
-    price: 10
-  },
-  {
-    name: "Book",
-    price: 5
-  },
-  {
-    name: "Phone",
-    price: 500
-  },
-  {
-    name: "Computer",
-    price: 1000
   }
 ]
 
 
 const total = items.reduce((acc, el) => {
+  console.log(`this the acc ${acc}`);
+  console.log(`this the el ${el}`);
   return acc + el.price
-});
+},0);
 
 
 console.log(total);
 
-// -> [object Object]2001055001000
 
 
-
-
-
-const myArr = [1, 2, 3, 4, 7, 5];
+const myArr = [5, 2, 3];
 
 
 const sum = myArr.reduce((acc, el) => {
+   console.log(`this the acc ${acc}`);
+   console.log(`this the el ${el}`);
   return acc + el
 
 });
@@ -49,47 +33,3 @@ const sum = myArr.reduce((acc, el) => {
 
 console.log(`The sum is ${sum}`);
 
-// -> 22
-
-
-const numbers = [1, -1, 2, 4]
-
-const newSum = numbers.reduce((accumulator, currentValue) => {
-  return accumulator + currentValue
-})
-
-console.log(`This is the result from Mosh' example: ${newSum}`)
-
-
-
-let data = [{
-    country: 'China',
-    pop: 1409517397,
-  },
-  {
-    country: 'India',
-    pop: 1339180127,
-  },
-  {
-    country: 'USA',
-    pop: 324459463,
-  },
-  {
-    country: 'Indonesia',
-    pop: 263991379,
-  }
-]
-
-
-let populationSum = data.reduce((acc, el) => {
-
-  return el.country === 'China' ? acc : acc + el.pop;
-  // if (el.country === 'China') {
-  //   return acc
-  // } else {
-  //   return acc + el.pop
-  // }
-
-}, 0);
-
-console.log(`Population sum not including China: ${populationSum}`);

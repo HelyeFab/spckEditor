be16bdf98d2dad7dee09f06c031d8dadf16919a9
HelyeFab <emmanuelfabiani23@gmail.com>
2021-01-13 21:08:13 +0000

    function SumOfTwo

--- 100Dev.js
+++ 100Dev.js
@@ -6,3 +6,13 @@ function doMath(a,b,c){
 }
 
 doMath(4,3,5)
+
+const numbers = [3,2,3,4,7]
+
+function sumOfTwo(){
+  let one = numbers[0];
+  let two = numbers[numbers.length - 1];
+  console.log( one + two);
+  
+}
+sumOfTwo();
